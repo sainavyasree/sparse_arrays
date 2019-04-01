1061a6c091a211e5beeff23447dd712aef0f12cb
sainavyasree <sainavyasreechikkala@gmail.com>
2019-04-01 20:53:47 +0530

    testClass

---
+++ TestClass.java
@@ -0,0 +1,35 @@
+import sparse_arrays.*;
+import java.util.*;
+
+public class TestClass {
+    
+    public static void main(String[] args) {
+        Scanner ob=new Scanner(System.in);
+        sparse_arrays sa=new sparse_arrays();
+
+        int stringsCount,queriesCount;
+        stringsCount=ob.nextInt();
+        String[] strings = new String[stringsCount];
+
+        for (int i = 0; i < stringsCount; i++) {
+            String s=ob.next();
+        strings[i]=s;
+        }
+
+        queriesCount = ob.nextInt();
+        String[] queries = new String[queriesCount];
+
+        for (int i = 0; i < queriesCount; i++) {
+            queries[i] = ob.next();
+        }
+    
+
+        int[] res = sa.matchingStrings(strings, queries);
+
+        for (int i = 0; i < res.length; i++) {
+            System.out.println(res[i]);
+        }
+        ob.close();
+    }
+}
+
